d121bf2f29ebdc36a0b91e1246cc70715f6a64ab
Henrik Karapetyan <g.karapetyan@tool-kit.tech>
2024-06-08 14:55:04 +0400

    addec services

--- src/ConsoleComponent.php
+++ src/ConsoleComponent.php
@@ -4,8 +4,15 @@ namespace Henrik\Console;
 
 use Henrik\Console\AttributeParsers\AsCommandAttributeParser;
 use Henrik\Console\Attributes\AsCommand;
+use Henrik\Console\Interfaces\CommandRunnerInterface;
+use Henrik\Console\Interfaces\InputInterface;
+use Henrik\Console\Interfaces\OutputInterface;
+use Henrik\Console\IO\Input;
+use Henrik\Console\IO\Output;
+use Henrik\Console\Subscribers\CommandEventSubscriber;
 use Henrik\Contracts\BaseComponent;
 use Henrik\Contracts\ComponentInterfaces\AttributesAndParsersAwareInterface;
+use Henrik\Contracts\Console\CommandProcessorInterface;
 use Henrik\Contracts\Console\CommandsContainerInterface;
 use Henrik\Contracts\Enums\ServiceScope;
 
@@ -18,6 +25,26 @@ class ConsoleComponent extends BaseComponent implements AttributesAndParsersAwar
                 [
                     'id'    => CommandsContainerInterface::class,
                     'class' => CommandsContainer::class,
+                ],
+                [
+                    'id'    => CommandProcessorInterface::class,
+                    'class' => CommandProcessor::class,
+                ],
+                [
+                    'id'    => CommandRunnerInterface::class,
+                    'class' => CommandRunner::class,
+                ],
+                [
+                    'id'    => InputInterface::class,
+                    'class' => Input::class,
+                ],
+                [
+                    'id'    => OutputInterface::class,
+                    'class' => Output::class,
+                ],
+                [
+                    'id'    => CommandEventSubscriber::class,
+                    'class' => CommandEventSubscriber::class,
                 ],
             ],
         ];
