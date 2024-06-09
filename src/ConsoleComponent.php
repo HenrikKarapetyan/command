7132a45d3b992af6363e091eca18f9501af32c96
Henrik Karapetyan <g.karapetyan@tool-kit.tech>
2024-06-09 10:27:30 +0400

    added implementattion for on bootstrap events interface

--- src/ConsoleComponent.php
+++ src/ConsoleComponent.php
@@ -4,6 +4,7 @@ namespace Henrik\Console;
 
 use Henrik\Console\AttributeParsers\AsCommandAttributeParser;
 use Henrik\Console\Attributes\AsCommand;
+use Henrik\Console\Interfaces\CommandInterface;
 use Henrik\Console\Interfaces\CommandRunnerInterface;
 use Henrik\Console\Interfaces\InputInterface;
 use Henrik\Console\Interfaces\OutputInterface;
@@ -12,11 +13,14 @@ use Henrik\Console\IO\Output;
 use Henrik\Console\Subscribers\CommandEventSubscriber;
 use Henrik\Contracts\BaseComponent;
 use Henrik\Contracts\ComponentInterfaces\AttributesAndParsersAwareInterface;
+use Henrik\Contracts\ComponentInterfaces\OnBootstrapAwareInterface;
 use Henrik\Contracts\Console\CommandProcessorInterface;
 use Henrik\Contracts\Console\CommandsContainerInterface;
+use Henrik\Contracts\CoreEvents;
 use Henrik\Contracts\Enums\ServiceScope;
+use Henrik\Events\EventDispatcher;
 
-class ConsoleComponent extends BaseComponent implements AttributesAndParsersAwareInterface
+class ConsoleComponent extends BaseComponent implements AttributesAndParsersAwareInterface, OnBootstrapAwareInterface
 {
     public function getServices(): array
     {
@@ -46,6 +50,10 @@ class ConsoleComponent extends BaseComponent implements AttributesAndParsersAwar
                     'id'    => CommandEventSubscriber::class,
                     'class' => CommandEventSubscriber::class,
                 ],
+                [
+                    'id'    => CoreEvents::COMMAND_DISPATCHER_DEFAULT_DEFINITION_ID,
+                    'class' => EventDispatcher::class,
+                ],
             ],
         ];
     }
@@ -54,6 +62,15 @@ class ConsoleComponent extends BaseComponent implements AttributesAndParsersAwar
     {
         return [
             AsCommand::class => AsCommandAttributeParser::class,
+        ];
+    }
+
+    public function onBootstrapDispatchEvents(): array
+    {
+        return [
+            CoreEvents::COMMAND_DISPATCHER_DEFAULT_DEFINITION_ID => [
+                CommandInterface::class => CoreEvents::COMMAND_MATCH_EVENTS,
+            ],
         ];
     }
 }
