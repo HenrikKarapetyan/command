8fceb499fdf70beab70b5b27549ebca4d1de3966
Henrik Karapetyan <g.karapetyan@tool-kit.tech>
2024-06-09 10:42:40 +0400

    fixed bug

--- src/ConsoleComponent.php
+++ src/ConsoleComponent.php
@@ -4,7 +4,6 @@ namespace Henrik\Console;
 
 use Henrik\Console\AttributeParsers\AsCommandAttributeParser;
 use Henrik\Console\Attributes\AsCommand;
-use Henrik\Console\Interfaces\CommandInterface;
 use Henrik\Console\Interfaces\CommandRunnerInterface;
 use Henrik\Console\Interfaces\InputInterface;
 use Henrik\Console\Interfaces\OutputInterface;
@@ -13,14 +12,13 @@ use Henrik\Console\IO\Output;
 use Henrik\Console\Subscribers\CommandEventSubscriber;
 use Henrik\Contracts\BaseComponent;
 use Henrik\Contracts\ComponentInterfaces\AttributesAndParsersAwareInterface;
-use Henrik\Contracts\ComponentInterfaces\OnBootstrapAwareInterface;
 use Henrik\Contracts\Console\CommandProcessorInterface;
 use Henrik\Contracts\Console\CommandsContainerInterface;
 use Henrik\Contracts\CoreEvents;
 use Henrik\Contracts\Enums\ServiceScope;
 use Henrik\Events\EventDispatcher;
 
-class ConsoleComponent extends BaseComponent implements AttributesAndParsersAwareInterface, OnBootstrapAwareInterface
+class ConsoleComponent extends BaseComponent implements AttributesAndParsersAwareInterface
 {
     public function getServices(): array
     {
@@ -62,15 +60,6 @@ class ConsoleComponent extends BaseComponent implements AttributesAndParsersAwar
     {
         return [
             AsCommand::class => AsCommandAttributeParser::class,
-        ];
-    }
-
-    public function onBootstrapDispatchEvents(): array
-    {
-        return [
-            CoreEvents::COMMAND_DISPATCHER_DEFAULT_DEFINITION_ID => [
-                CommandInterface::class => CoreEvents::COMMAND_MATCH_EVENTS,
-            ],
         ];
     }
 }
